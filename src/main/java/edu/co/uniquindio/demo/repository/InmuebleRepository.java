04ce20b9ef8a981d3ef9aebccb2be0e3958d0e18
Daniela Castañeda <159925105+DanielaCC27@users.noreply.github.com>
2025-10-06 12:57:34 -0500

    Agrega consulta por propietarioId

--- src/main/java/edu/co/uniquindio/demo/repository/InmuebleRepository.java
+++ src/main/java/edu/co/uniquindio/demo/repository/InmuebleRepository.java
@@ -3,7 +3,9 @@ package edu.co.uniquindio.demo.repository;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import edu.co.uniquindio.demo.model.Inmueble;
+import org.bson.types.ObjectId;
 
 public interface InmuebleRepository extends MongoRepository<Inmueble, String> {
     List<Inmueble> findByEstado(String estado);
-}
+    List<Inmueble> findByPropietarioId(ObjectId propietarioId); //Consulta por propietario
+} 
